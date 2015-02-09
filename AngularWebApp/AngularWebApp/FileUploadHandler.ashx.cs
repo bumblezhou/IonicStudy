6a5af6de210d15d61fb56599aa9ff4fa0d082b97
bumblezhou <bumble.zhou@gmail.com>
2015-02-09 15:42:31 +0800

    添加一个ASP.NET编写的文件上传的服务端。

---
+++ AngularWebApp/AngularWebApp/FileUploadHandler.ashx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace AngularWebApp
+{
+    /// <summary>
+    /// Summary description for FileUploadHandler
+    /// </summary>
+    public class FileUploadHandler : IHttpHandler
+    {
+        private JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //context.Response.ContentType = "text/plain";
+            context.Response.ContentType = "application/json";
+            if (context.Request["user"] != null
+                && context.Request["user"] == "zhou"
+                //&& context.Request["password"] != null
+                //&& context.Request["password"] == "iloveyou@1014"
+                && context.Request["action"] != null
+                && context.Request["action"] == "u")
+            {
+                if (context.Request.Files.Count > 0)
+                {
+                    // Get the uploaded image from the Files collection
+                    var httpPostedFile = HttpContext.Current.Request.Files[0];
+                    // Validate the uploaded image(optional)
+
+                    // Get the complete file path
+                    var fileSavePath = GenerateUploadFileName(context.Server.MapPath("~/FileUploads"));
+                    //var fileSavePath = Path.Combine(context.Server.MapPath("~/FileUploads"), httpPostedFile.FileName);
+
+                    // Save the uploaded file to "UploadedFiles" folder
+                    httpPostedFile.SaveAs(fileSavePath);
+
+                    context.Response.Write(javaScriptSerializer.Serialize(new { Result = true, Message = "File upload sucessfully!" }));
+                }
+                else
+                {
+                    context.Response.Write(javaScriptSerializer.Serialize(new { Result = false, Message = "No image file found to upload." }));
+                }
+            }
+            else
+            {
+                context.Response.Write(javaScriptSerializer.Serialize(new { Result = false, Message = "You are not a validate user." }));
+            }
+        }
+
+        public string GenerateUploadFileName(string rootFolderFullPath)
+        {
+            if (!Directory.Exists(rootFolderFullPath))
+            {
+                Directory.CreateDirectory(rootFolderFullPath);
+            }
+
+            var subFolderName = DateTime.Now.ToString("yyyy-MM-dd");
+            var subFolderFullPath = Path.Combine(rootFolderFullPath, subFolderName);
+            if (!Directory.Exists(subFolderFullPath))
+            {
+                Directory.CreateDirectory(subFolderFullPath);
+            }
+
+            var fileName = Guid.NewGuid().ToString() + ".JPG";
+            var fileFullPath = Path.Combine(subFolderFullPath, fileName);
+            while (Directory.Exists(fileFullPath))
+            {
+                fileName = Guid.NewGuid().ToString() + ".JPG";
+                fileFullPath = Path.Combine(subFolderFullPath, fileName);
+            }
+            return fileFullPath;
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AngularWebApp/AngularWebApp/Scripts/app.js
@@ -0,0 +1,35 @@
+﻿//inject angular file upload directives and service.
+angular.module('myApp', ['angularFileUpload'])
+.controller('UploadCtrl', function ($scope, $upload) {
+    //$scope.$watch('files', function () {
+    //    $scope.myFormData = {user: "zhou", action: "u"};
+    //    $scope.upload = $upload.upload({
+    //        url: 'FileUploadHandler.ashx',
+    //        data: $scope.myFormData,
+    //        file: $scope.files
+    //    }).progress(function (evt) {
+    //        
+    //    }).success(function (data, status, headers, config) {
+    //        console.log(data.Message);
+    //    });
+    //});
+    $scope.myFormData = { user: "zhou", action: "u" };
+    $scope.onFileSelect = function ($files) {
+        //$files: an array of files selected, each file has name, size, and type.
+        for (var i = 0; i < $files.length; i++) {
+            var file = $files[i];
+            $scope.upload = $upload.upload({
+                url: 'FileUploadHandler.ashx',
+                data: $scope.myFormData,
+                file: file, // or list of files ($files) for html5 only
+            }).progress(function (evt) {
+                //console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+            }).success(function (data, status, headers, config) {
+                //alert('Uploaded successfully ' + file.name);
+                console.log(data.Message);
+            }).error(function (err) {
+                //alert('Error occured during upload');
+            });
+        }
+    };
+});
