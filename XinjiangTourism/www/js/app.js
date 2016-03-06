8a7b90482727ec4c8ee83017c9f1312d805f92c8
bumble.zhou <bumble.zhou@gmail.com>
2016-03-07 01:07:01 +0800

    完成移动社区和去新疆两个应用的的界面改造，并进行上架发布。

--- XinjiangTourism/www/js/app.js
+++ XinjiangTourism/www/js/app.js
@@ -5,21 +5,43 @@
 // the 2nd parameter is an array of 'requires'
 var xinjiangTourismApp = angular.module('starter', ['ionic', 'ngCordova']);
 
-xinjiangTourismApp.run(function($ionicPlatform) {
+xinjiangTourismApp.run(function($ionicPlatform, $cordovaGeolocation, $interval) {
   $ionicPlatform.ready(function() {
+
+    //设置键盘属性
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
 
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
+
+    //设置状态栏属性
     if(window.StatusBar) {
-      StatusBar.styleDefault();
+      //StatusBar.styleDefault();
+      StatusBar.hide();
     }
+
+    //获取地理坐标开始
+    var posOptions = {timeout: 10000, enableHighAccuracy: true};
+    var getLocation = function () {
+      $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function (position) {
+          console.log(position.coords);
+        }, function(err) {
+          // error'
+          console.log(err);
+        });
+    };
+    getLocation();
+    //$interval(getLocation, 3000); // 3 seconds for now
+    //获取地理坐标结束
+
   });
 });
 
@@ -29,7 +51,7 @@ xinjiangTourismApp.controller('IndexCtrl', function($scope, $state, $timeout, $i
     template: '<img src="img/loading.gif" alt="加载中..." />'
   });
 
-  $('<iframe id="nav-view-main-frame" src="http://www.qxj.wap.klmyqsh.com/xshop/shopHome.php" style="min-height:90vh;" height="100%" width="100%" frameborder="0"></iframe>').appendTo('#main_content .scroll');
+  $('<iframe id="nav-view-main-frame" src="http://www.qxj.wap.klmyqsh.com/xshop/shopHome.php" style="min-height:100vh;" height="100%" width="100%" frameborder="0"></iframe>').appendTo('#main_content .scroll');
   $("#nav-view-main-frame").load(function(){
     $ionicLoading.hide();
   });
